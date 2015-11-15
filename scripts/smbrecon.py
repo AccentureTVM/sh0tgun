717c696e2e9c8433d8eeb92c01108609021a8c08
taapes <tapettis@gmail.com>
2015-11-15 13:35:07 -0800

    print check

--- scripts/smbrecon.py
+++ scripts/smbrecon.py
@@ -18,6 +18,7 @@ def main(args):
 	nbtresults = subprocess.check_output(NBTSCAN, shell=True, stderr=subprocess.STDOUT)
 	print(nbtresults)
 	if "Connection refused" not in nbtresults and "Connect error" not in nbtresults and "Connection reset" not in nbtresults:
+		print ("CHECK")
 		print("FOUND: NBTSCAN User accounts/domains found on " + ip + " check discovery/smb for results")
 		resultsfile = root + "discovery/smb/" + ip + "_nbtscan.txt"
 		f = open(resultsfile, "w+")
