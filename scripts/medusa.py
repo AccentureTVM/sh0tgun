281a0452add888abee272fff8b676c09f262d3b7
root <root@localhost.localdomain>
2015-12-03 18:30:13 -0500

    medusa fix

--- scripts/medusa.py
+++ scripts/medusa.py
@@ -61,16 +61,19 @@ def webformCrack(ip_address, port, root, options):
 	return
 
 def ftpCrack(ip_address, port, root, options):
-	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M mssql > " + root + "password/mssql/medusa_" + ip_address + ".txt"
+	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M ftp > " + root + "password/ftp/medusa_" + ip_address + ".txt"
 	print (MEDUSA)
 	results = subprocess.check_output(MEDUSA, shell=True)
 	results = results.decode('utf-8')
-	results = results.split("\n")
+	fr = open(root + "password/ftp/medusa_" + ip_address + ".txt")
 	fo = open(root + "password/passwords.csv", 'w+')
-	for line in results:
+	for line in fr:
 		if "SUCCESS" in line:
-			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+line)
-			fo.write(ip_address + "," + port + "," + line + "," + ftp + "\n")
+			line = line.split(" ")
+			un = line[6]
+			pw = line[8]
+			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+un + "/" + pw)
+			fo.write(ip_address + "," + port + "," + un + "," + pw + ",ftp\n")
 	fo.close()
 	return
 
@@ -92,4 +95,10 @@ if __name__=='__main__':
 	if sys.version_info[0] != 3 or sys.version_info[1] < 1:
 		print("\nEXIT: This script requires Python version 3.1 or higher\n")
 		sys.exit(1)
-	mssqlCrack("192.168.1.248", "1433", "/root/TEST/")
+	medusaOptions = {
+		"users":"-U /root/sh0tgun/wordlists/test.txt",
+		"pws": "-P /root/sh0tgun/wordlists/test.txt",
+		"jb": "-e ns",
+		"verbosity": "6",
+	}
+	ftpCrack("192.168.1.50", "21", "/root/TEST/",medusaOptions)
