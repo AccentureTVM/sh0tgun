7d20d72a2dd504ea17963dbfd83b4f60ad696f55
taapes <tapettis@gmail.com>
2015-12-03 15:36:16 -0800

    medusa

--- scripts/medusa.py
+++ scripts/medusa.py
@@ -4,65 +4,72 @@ import subprocess
 import sys
 
 def sshCrack(ip_address, port, root, options):
-	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M mssql > " + root + "password/mssql/medusa_" + ip_address + ".txt"
-	print (MEDUSA)
+	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M ssh > " + root + "password/ssh/medusa_" + ip_address + ".txt"
 	results = subprocess.check_output(MEDUSA, shell=True)
 	results = results.decode('utf-8')
-	results = results.split("\n")
+	fr = open(root + "password/ssh/medusa_" + ip_address + ".txt")
 	fo = open(root + "password/passwords.csv", 'w+')
-	for line in results:
+	for line in fr:
 		if "SUCCESS" in line:
-			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+line)
-			fo.write(ip_address + "," + port + "," + line + "," + ssh + "\n")
+			line = line.split(" ")
+			un = line[6]
+			pw = line[8]
+			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+un + "/" + pw)
+			fo.write(ip_address + "," + port + "," + un + "," + pw + ",ssh\n")
+	fr.close()
 	fo.close()
-	return
 
 
 def mssqlCrack(ip_address, port, root, options):
 	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M mssql > " + root + "password/mssql/medusa_" + ip_address + ".txt"
-	print (MEDUSA)
 	results = subprocess.check_output(MEDUSA, shell=True)
 	results = results.decode('utf-8')
-	results = results.split("\n")
+	fr = open(root + "password/mssql/medusa_" + ip_address + ".txt")
 	fo = open(root + "password/passwords.csv", 'w+')
-	for line in results:
+	for line in fr:
 		if "SUCCESS" in line:
-			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+line)
-			fo.write(ip_address + "," + port + "," + line + "," + mssql + "\n")
+			line = line.split(" ")
+			un = line[6]
+			pw = line[8]
+			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+un + "/" + pw)
+			fo.write(ip_address + "," + port + "," + un + "," + pw + ",mssql\n")
+	fr.close()
 	fo.close()
-	return
 
 def mysqlCrack(ip_address, port, root, options):
-	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M mssql > " + root + "password/mssql/medusa_" + ip_address + ".txt"
-	print (MEDUSA)
+	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M mysql > " + root + "password/mysql/medusa_" + ip_address + ".txt"
 	results = subprocess.check_output(MEDUSA, shell=True)
 	results = results.decode('utf-8')
-	results = results.split("\n")
+	fr = open(root + "password/mysql/medusa_" + ip_address + ".txt")
 	fo = open(root + "password/passwords.csv", 'w+')
-	for line in results:
+	for line in fr:
 		if "SUCCESS" in line:
-			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+line)
-			fo.write(ip_address + "," + port + "," + line + "," + mysql + "\n")
+			line = line.split(" ")
+			un = line[6]
+			pw = line[8]
+			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+un + "/" + pw)
+			fo.write(ip_address + "," + port + "," + un + "," + pw + ",mysql\n")
+	fr.close()
 	fo.close()
-	return
-
+	
 def webformCrack(ip_address, port, root, options):
-	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M mssql > " + root + "password/mssql/medusa_" + ip_address + ".txt"
-	print (MEDUSA)
+	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M web-form > " + root + "password/http/medusa_" + ip_address + ".txt"
 	results = subprocess.check_output(MEDUSA, shell=True)
 	results = results.decode('utf-8')
-	results = results.split("\n")
+	fr = open(root + "password/http/medusa_" + ip_address + ".txt")
 	fo = open(root + "password/passwords.csv", 'w+')
-	for line in results:
+	for line in fr:
 		if "SUCCESS" in line:
-			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+line)
-			fo.write(ip_address + "," + port + "," + line + "," + web + "\n")
+			line = line.split(" ")
+			un = line[6]
+			pw = line[8]
+			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+un + "/" + pw)
+			fo.write(ip_address + "," + port + "," + un + "," + pw + ",http\n")
+	fr.close()
 	fo.close()
-	return
 
 def ftpCrack(ip_address, port, root, options):
 	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M ftp > " + root + "password/ftp/medusa_" + ip_address + ".txt"
-	print (MEDUSA)
 	results = subprocess.check_output(MEDUSA, shell=True)
 	results = results.decode('utf-8')
 	fr = open(root + "password/ftp/medusa_" + ip_address + ".txt")
@@ -74,22 +81,24 @@ def ftpCrack(ip_address, port, root, options):
 			pw = line[8]
 			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+un + "/" + pw)
 			fo.write(ip_address + "," + port + "," + un + "," + pw + ",ftp\n")
+	fr.close()
 	fo.close()
-	return
 
 def vncCrack(ip_address, port, root, options):
-	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M mssql > " + root + "password/mssql/medusa_" + ip_address + ".txt"
-	print (MEDUSA)
+	MEDUSA = "medusa -h " + ip_address + " " + options["users"] + " " + options["pws"] +  " -v" + options["verbosity"] + " -n " + port + " " + options["jb"] + " -M vnc > " + root + "password/vnc/medusa_" + ip_address + ".txt"
 	results = subprocess.check_output(MEDUSA, shell=True)
 	results = results.decode('utf-8')
-	results = results.split("\n")
+	fr = open(root + "password/vnc/medusa_" + ip_address + ".txt")
 	fo = open(root + "password/passwords.csv", 'w+')
-	for line in results:
+	for line in fr:
 		if "SUCCESS" in line:
-			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+line)
-			fo.write(ip_address + "," + port + "," + line + "," + vnc + "\n")
+			line = line.split(" ")
+			un = line[6]
+			pw = line[8]
+			print("PASSWORD FOUND for "+ip_address+":"+port+ " - "+un + "/" + pw)
+			fo.write(ip_address + "," + port + "," + un + "," + pw + ",vnc\n")
+	fr.close()
 	fo.close()
-	return
 	
 if __name__=='__main__':
 	if sys.version_info[0] != 3 or sys.version_info[1] < 1:
